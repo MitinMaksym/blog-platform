b813a13ace87df4e4e2b835c888e9eaa91c4c733
Maks <maksim.mitin@powercode.us>
2023-05-14 20:13:31 +0300

    feat: add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import { Suspense } from 'react'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { classNames } from 'shared/lib/classNames/classNames'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { AppRouter } from 'app/providers/Router'
 
 import './styles/index.scss'
-import { useTheme } from 'app/providers/ThemeProvider'
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme()
@@ -16,12 +13,7 @@ export const App = () => {
       <Link to='/'>Main</Link>
       <Link to='/about'>About</Link>
       <button onClick={toggleTheme}>Toggle Theme</button>
-      <Suspense fallback={<div>Loading</div>}>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   )
 }
