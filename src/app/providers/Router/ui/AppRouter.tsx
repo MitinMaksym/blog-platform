1bee89863ddc08bcc4dfa4f091d94b94f229eeb2
Maksym Mitin <mitinmaksym@gmail.com>
2023-05-17 17:04:40 +0300

    feat: add PageLoader component

--- src/app/providers/Router/ui/AppRouter.tsx
+++ src/app/providers/Router/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routerConfig } from 'shared/config/routerConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 export const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PageLoader/>}>
         <Routes>
             {Object.values(routerConfig).map(({ path, element }) => (
                 <Route
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import  './Loader.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader: FC<LoaderProps> = ({ className }) => (
+    <div className={classNames('ldsEllipsis', {}, [className])}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+
+);
