948ab923811302b0bbb996d95f4cffd3f3a4e5bc
Maksym Mitin <mitinmaksym@gmail.com>
2023-05-23 14:46:58 +0300

    feat: update modal dark

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -26,6 +26,9 @@ export const Light: Story = {
 
 export const Dark: Story = {
     args: {
+        children: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Nulla consequuntur sequi harum, earum accusamus esse!`
+
     },
     decorators: [ThemeDecorator(Theme.DARK)]
 };
