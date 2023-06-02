fd176afd4a5d09d5f5b39ce434fbeb79a203dd7b
Maksym Mitin <mitinmaksym@gmail.com>
2023-06-02 16:18:25 +0300

    feat: fix build

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,4 +1,4 @@
 export { selectUserAuthData } from './selectors/selectUserAuthData/selectUserAuthData';
-export { userActions } from './model/slice/userSlice';
-export { userReducer } from './model/slice/userSlice';
+export { userActions } from './model/slice/usserSlice';
+export { userReducer } from './model/slice/usserSlice';
 export { UserSchema, User } from './model/types/userSchema';
