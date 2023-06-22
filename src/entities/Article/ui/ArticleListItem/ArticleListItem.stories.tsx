e0b71870c4c005785ad23fd0f2d5ce6c811ae7d3
Maksym Mitin <mitinmaksym@gmail.com>
2023-06-22 18:00:58 +0300

    feat: test ci

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.stories.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.stories.tsx
@@ -6,54 +6,22 @@ import { Article } from '../../model/types/article';
 import { ArticleListItem } from './ArticleListItem';
 
 
-
-
 const article = {
     'id': '1',
     'title': 'Javascript news',
     'subtitle': 'Что нового в JS за 2022 год?',
-    'img': testImg,
+    'img': '',
     'views': 1022,
     'createdAt': '26.02.2022',
     'user':    {
         'id': '1',
         'username': 'admin',
-        'avatar': avatarImg
+        'avatar': ''
     },
     'type': [
         'IT', 'SCIENCE', 'SCIENCE', 'IT'
     ],
     'blocks': [
-        {
-            'id': '1',
-            'type': 'TEXT',
-            'title': 'Заголовок этого блока',
-            'paragraphs': [
-                // eslint-disable-next-line
-                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
-            ]
-        },
-        {
-            'id': '4',
-            'type': 'CODE',
-            // eslint-disable-next-line
-            'code': '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;'
-        },
-        {
-            'id': '5',
-            'type': 'TEXT',
-            'title': 'Заголовок этого блока',
-            'paragraphs': [
-                // eslint-disable-next-line
-                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
-            ]
-        },
-        {
-            'id': '3',
-            'type': 'CODE',
-            // eslint-disable-next-line
-            'code': "const path = require('path');\n\nconst server = jsonServer.create();\n\nconst router = jsonServer.router(path.resolve(__dirname, 'db.json'));\n\nserver.use(jsonServer.defaults({}));\nserver.use(jsonServer.bodyParser);"
-        },
         {
             'id': '7',
             'type': 'TEXT',
@@ -63,15 +31,6 @@ const article = {
                 'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
             ]
         },
-        {
-            'id': '9',
-            'type': 'TEXT',
-            'title': 'Заголовок этого блока',
-            'paragraphs': [
-                // eslint-disable-next-line
-                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.'
-            ]
-        }
     ]
 } as Article;
 
@@ -84,7 +43,7 @@ const meta: Meta<typeof ArticleListItem> = {
     },
     decorators:[withRouter],
     parameters: {
-        loki: { skip: true }, // TODO: figure out why screenshots fails in ci
+        // loki: { skip: true }, // TODO: figure out why screenshots fails in ci
     }  
 };
 
